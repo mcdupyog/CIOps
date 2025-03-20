8f0706b4b1a25d8d604941085cc7ff8a329a560f
mcd89 <mcdupyog@gmail.com>
2025-03-20 15:31:06 +0530

    Update jobBuilder.groovy

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -29,7 +29,7 @@ spec:
             name: jenkins-credentials
             key: dockerPassword
       - name: DOCKER_NAMESPACE
-        value: nudmcdg
+        value: mcd89
       - name: DOCKER_GROUP_NAME  
         value: dev
     resources:
